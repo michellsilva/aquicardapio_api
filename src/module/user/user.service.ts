3dbe40ce4f00e4db2986d10dcc361e646b5ce7a5
michellsilva <michell3512206@hotmail.com>
2024-03-17 21:49:48 -0300

    criando projeto inicial

---
+++ src/module/user/user.service.ts
@@ -0,0 +1,77 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/database/prismaService';
+
+@Injectable()
+export class UserService {
+
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: CreateUserDto) {
+    const userExists = await this.prisma.user.findUnique({
+      where: {
+        email: data.email,
+      }
+    });
+
+    if(userExists)
+      throw new Error('Usuário já cadastrado para este e-mail.');
+
+    const user = await this.prisma.user.create({
+      data,
+    });
+
+    return user;
+  }
+
+  async findAll() {
+    return await this.prisma.user.findMany();
+  }
+
+  async findOne(id: string) {
+    const user = await this.prisma.user.findFirst({
+      where: {
+        id: id
+      }
+    });
+
+    return user;
+  }
+
+  async update(id: string, data: UpdateUserDto) {
+    const userExists = await this.prisma.user.findUnique({
+      where: {
+        id: id,
+      }
+    });
+
+    if(!userExists)
+      throw new Error('O Usuário não encotrado.');
+
+    await this.prisma.user.update({
+      data,
+      where: {
+        id: id
+      }
+    });
+
+  }
+
+  async remove(id: string) {
+    const userExists = await this.prisma.user.findUnique({
+      where: {
+        id: id,
+      }
+    });
+
+    if(!userExists)
+      throw new Error('O Usuário não encotrado.');
+
+    await this.prisma.user.delete({
+      where:{
+        id: id
+      }
+    });
+  }
+}
