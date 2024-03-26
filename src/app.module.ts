c658a08ea9686543fe057f281d3cc9a696681211
michellsilva <michell3512206@hotmail.com>
2024-03-26 00:34:20 -0300

    Adicionado limit request

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,23 @@ import { AuthModule } from './module/auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './module/auth/guards/jwt-auth.guard';
 import { RolesGuard } from './module/auth/guards/roles.guard';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [UserModule, LicenseModule, AuthModule],
+  imports: [
+    UserModule,
+    LicenseModule,
+    AuthModule,
+    ThrottlerModule.forRoot([{
+      ttl: 1000,
+      limit: 1,
+    }])
+  ],
   controllers: [AppController],
   providers: [
     { provide: APP_GUARD, useClass: JwtAuthGuard },
-    { provide: APP_GUARD, useClass: RolesGuard }
+    { provide: APP_GUARD, useClass: RolesGuard },
+    { provide: APP_GUARD, useClass: ThrottlerGuard },
   ],
 })
 export class AppModule {}
